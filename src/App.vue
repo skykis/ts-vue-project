3bd1c19e0acd5ff4239da770433bc0c23b5d5b3d
wbx16744 <wangbeixing@gopherasset.com>
2022-08-22 14:05:34 +0800

    删减

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,17 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import NavigationVue from './components/Navigation.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <el-container>
+    <el-header>
+      <NavigationVue/>
+    </el-header>
+    <el-main>
+      <RouterView />
+    </el-main>
+  </el-container>
 </template>
 
 <style scoped>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <el-row>
+        <el-col :span="24">
+            <el-carousel :interval="4000" type="card" height="200px">
+                <el-carousel-item v-for="item in 6" :key="item">
+                    <h3 text="2xl" justify="center">{{ item }}</h3>
+                </el-carousel-item>
+            </el-carousel>
+        </el-col>
+    </el-row>
+</template>
+
+<style scoped>
+.el-row {
+    margin-bottom: 20px;
+    width: 100%;
+    height: 100%;
+}
+
+.el-row:last-child {
+    margin-bottom: 0;
+}
+
+.el-col {
+    border-radius: 4px;
+}
+
+.el-carousel__item h3 {
+    color: #475669;
+    opacity: 0.75;
+    line-height: 200px;
+    margin: 0;
+    text-align: center;
+}
+
+.el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+    background-color: #d3dce6;
+}
+</style>
